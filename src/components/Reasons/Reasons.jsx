32f42e015313bc42b937dd0f2baa33b23bd50996
kylie <wlswn4630@gmail.com>
2022-08-03 19:13:31 +0900

    feat: Reasons component

--- src/components/Reasons/Reasons.jsx
+++ src/components/Reasons/Reasons.jsx
@@ -18,7 +18,37 @@ export const Reasons = () => {
         <img src={image3} alt="the man who exercise"></img>
         <img src={image4} alt="the man who exercise"></img>
       </div>
-      <div className="right-r"></div>
+      <div className="right-r">
+        <span>some reasons</span>
+        <div>
+          <span className="stroke-text">why</span>
+          <span> choose us?</span>
+        </div>
+        <div className="details-r">
+          <div>
+            <img src={tick} alt=""></img>
+            <span>OVER 140+ EXPERT COACHS</span>
+          </div>
+          <div>
+            <img src={tick} alt=""></img>
+            <span>TRAIN SMARTER AND FASTER THAN BEFORE</span>
+          </div>
+          <div>
+            <img src={tick} alt=""></img>
+            <span>1 FREE PROGRAM FOR NEW MEMBER</span>
+          </div>
+          <div>
+            <img src={tick} alt=""></img>
+            <span>RELIABLE PARTNER</span>
+          </div>
+        </div>
+        <span id="partner-title">OUR PARTNERS</span>
+        <div className="partners">
+          <img src={nb}></img>
+          <img src={adidas}></img>
+          <img src={nike}></img>
+        </div>
+      </div>
     </div>
   );
 };

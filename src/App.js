dffa5fccc3fce90671db93804fe250ef57e8d9b3
kylie <wlswn4630@gmail.com>
2022-08-03 19:52:42 +0900

    feat: Plans component

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import "./App.css";
 import { Hero } from "./components/Hero/Hero";
 import { Progrmas } from "./components/Programs/Progrmas";
 import { Reasons } from "./components/Reasons/Reasons";
+import { Plans } from "./components/Plans/Plans";
 function App() {
   return (
     <div className="App">
       <Hero />
       <Progrmas />
       <Reasons />
+      <Plans />
     </div>
   );
 }
--- src/components/Reasons/Reasons.jsx
+++ src/components/Reasons/Reasons.jsx
@@ -26,27 +26,27 @@ export const Reasons = () => {
         </div>
         <div className="details-r">
           <div>
-            <img src={tick} alt=""></img>
+            <img src={tick} alt="target"></img>
             <span>OVER 140+ EXPERT COACHS</span>
           </div>
           <div>
-            <img src={tick} alt=""></img>
+            <img src={tick} alt="target"></img>
             <span>TRAIN SMARTER AND FASTER THAN BEFORE</span>
           </div>
           <div>
-            <img src={tick} alt=""></img>
+            <img src={tick} alt="target"></img>
             <span>1 FREE PROGRAM FOR NEW MEMBER</span>
           </div>
           <div>
-            <img src={tick} alt=""></img>
+            <img src={tick} alt="target"></img>
             <span>RELIABLE PARTNER</span>
           </div>
         </div>
         <span id="partner-title">OUR PARTNERS</span>
         <div className="partners">
-          <img src={nb}></img>
-          <img src={adidas}></img>
-          <img src={nike}></img>
+          <img src={nb} alt="logo"></img>
+          <img src={adidas} alt="logo"></img>
+          <img src={nike} alt="logo"></img>
         </div>
       </div>
     </div>

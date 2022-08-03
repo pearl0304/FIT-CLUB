e17753963461879d33fb42fedab3c2b97074e819
Kylie <kylie@comets.kr>
2022-08-03 15:29:24 +0900

    style: reasons

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import "./App.css";
 import { Hero } from "./components/Hero/Hero";
 import { Progrmas } from "./components/Programs/Progrmas";
+import { Reasons } from "./components/Reasons/Reasons";
 function App() {
   return (
     <div className="App">
       <Hero />
       <Progrmas />
+      <Reasons />
     </div>
   );
 }
